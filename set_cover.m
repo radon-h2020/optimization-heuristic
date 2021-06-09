575dfb618625f1f73079b63c24435e10bcfd595c
Eleftherios Anastasiadis <eanastas@ic.ac.uk>
2021-06-09 17:32:52 +0100

    modikfy tosca model method

--- set_cover.m
+++ set_cover.m
@@ -1,7 +1,29 @@
-read_csv();
-%modify_tosca_model([pwd, filesep, 'test_model.tosca']);
+%  f=[5;10;3];
+%  a=2;
+% intcon = 1;
+% intcon = 2;
+% intcon = 3;
+% A = [-1,0,-1;
+%     0,-1,-1;
+%     -1,0,-1;
+%     -1,0,-1;
+%     0,-1,0;
+%     1,0,0;
+%     0,1,0;
+%     0,0,1];
+% b=[-1,-1,-1,-1,-1,1,1,1];
 
-function read_csv()
+%x = intlinprog(f,intcon,A,b)
+
+% tic;
+% generate_random_constraints();
+% toc;
+
+%[selected_sets, elements_by_set] = run_algorithm();
+%modify_tosca_model([pwd, filesep, 'test_model.tosca']);
+modify_tosca_model([pwd, filesep, 'model_In.tosca'], [pwd, filesep, 'model_Out.tosca']);
+
+function [selected_sets, elements_by_set] = run_algorithm()
     T1 = readtable('target_Test.csv');
     throughput_table = table2array(T1);
     T2 = readtable('input_Test.csv');
@@ -11,21 +33,20 @@ function read_csv()
     set_matrix = A.';
     tic
 %     HEURISTIC
-%     [selected_sets, elements_by_set] = greedy_set_cover(interference_table, set_matrix);
-%     disp(selected_sets)
-%     for i=1:length(elements_by_set)
-%         disp(elements_by_set(i).indexes)
-%     end
-%     a=2;
+    [selected_sets, elements_by_set] = greedy_set_cover(interference_table, set_matrix);
+    disp(selected_sets)
+    for i=1:length(elements_by_set)
+        disp(elements_by_set(i).indexes)
+    end
 
 %     EXACT
-    experiments = length(throughput_table);
-    b = ones([1, 24]);
-    intcon = 1:experiments;
-    f = throughput_table;
-    reshape(f,1,[]);
-    x = intlinprog(f,intcon,A,b);
-    toc
+%     experiments = length(throughput_table);
+%     b = ones([1, 24]);
+%     intcon = 1:experiments;
+%     f = throughput_table;
+%     reshape(f,1,[]);
+%     x = intlinprog(f,intcon,A,b);
+%     toc
 
 end
 
@@ -49,6 +70,48 @@ interference_table = [];
     end
 end
 
+
+function generate_random_constraints()
+    variables_n = 30;
+    constraints_n = 30;
+    
+    fileID = fopen('random_constraints.txt','w');
+    
+    fprintf(fileID,'f = [');
+    for i=1:variables_n
+        value=randsample([1:30],1);
+        fprintf(fileID,'%s;',num2str(value));
+    end
+    fprintf(fileID,'];\n\n\n');
+    
+    fprintf(fileID,'intcon = 1:%s;\n\n\n',num2str(variables_n));
+    
+    fprintf(fileID,'b = [');
+    for j=1:constraints_n
+        value=randsample([-1,1],1);
+        fprintf(fileID,'%s',num2str(value));
+        if j==constraints_n
+            fprintf(fileID,'];\n\n\n');
+        else
+            fprintf(fileID,',');
+        end
+    end
+    
+    fprintf(fileID,'A = [');
+    for j=1:constraints_n
+        for i=1:variables_n
+            value=randi([0 1]);
+            fprintf(fileID,'%s',num2str(value));
+            if i==variables_n
+                fprintf(fileID,';\n');
+            else
+                fprintf(fileID,',');
+            end
+        end
+    end
+    fprintf(fileID,'];');
+    fclose(fileID);
+end
 
 function f=generate_f(interference_table)
     f = interference_table
@@ -119,11 +182,32 @@ function [selected_sets, elements_by_set] = greedy_set_cover(set_weights, set_ma
 end 
 
 
-function modify_tosca_model(filename)
+function modify_tosca_model(filenameIn, filenameOut)
     addJarPaths();
     processor = YamlProcessor();
-    tosca = processor.read(filename);
+    tosca = processor.read(filenameIn);
     graph = TopologyGraph(tosca.topology_template);
-    a=2;
-
+    
+    [selected_sets, elements_by_set] = run_algorithm();
+    runtimeNodesNo = length(selected_sets);
+    
+%     allNodesStruct = graph.getNodes();
+%     allNodes = fieldnames(allNodesStruct);
+%     computeNodes = allNodes(startsWith(allNodes, 'Compute'));
+    
+    for i=1:runtimeNodesNo
+        runtimeNodeName = generateNodeName('ContainerRuntime_', graph);
+        runtimeNode = ContainerRuntime(runtimeNodeName);
+        graph.addNode(runtimeNode);
+        microservices = elements_by_set(i).indexes;
+        for j=1:length(microservices)
+            microserviceIndex = microservices(j);
+            computeNode = graph.getNode('Compute_' +string(microserviceIndex-1));
+            hostedOnRelationshipName = generateRelationshipName('HostedOn', graph);
+            hostedOnRelationship = HostedOn(hostedOnRelationshipName);
+            graph.addRelationship(hostedOnRelationship, runtimeNode, computeNode);
+        end
+    end
+    tosca.topology_template = graph.transformIntoTemplate();
+    processor.write(tosca, filenameOut);
 end
